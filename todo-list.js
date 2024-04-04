7d139d56ddac3b8744b3a2d8d6b42cef4844738b
VworpStageLeft <jmrdolgner@gmail.com>
2024-04-03 21:52:56 -0500

    implemented advanced options output.
    set labels to create new lists. made priority and due date visible on task items

--- todo-list.js
+++ todo-list.js
@@ -12,26 +12,49 @@ function toggleAdvanced() {
     }
 }
 
+//colors and iterator initialized outside the functions to not be reset ever time addTask is called.
+const listColor = ['#f5b7b7','#ffd482','#fff382','#86ff82','#8290ff','#cb82ff'];
+var i = 0;
+
 function addTask() {
-    var taskInput = document.getElementById('taskInput');
-    var taskList = document.getElementById('taskList');
+    //get all input information
+	var taskInput = document.getElementById('taskInput');
+	var taskLabel = document.getElementById('label');
+	var taskList = document.getElementById('default');
+	var taskPriority = document.getElementById('priority');
+	var taskDueDate = document.getElementById('dueDate');		
+	
 
     if (taskInput.value.trim() === '') {
         alert('Please enter a task.');
         return;
     }
 
+	//check for a label. add to datalist and create list if necessary
+	if (taskLabel.value.trim() != '') {
+        checkLabel(taskLabel, listColor[i]);
+		taskList = document.getElementById(taskLabel.value);
+		i++;
+		if(i >= listColor.length) i=0;
+    }
+	
+
     var newTask = document.createElement('li');
     newTask.innerHTML = `
-		<input type="checkbox" onclick="completeTask(this)">
-		<span>${taskInput.value}</span>
+		<input type="checkbox" onclick="completeTask(this)"> 
+		<span>${taskPriority.value} ${taskInput.value} </span> <span style="background: rgba(255, 255, 255, 0.5)">${taskDueDate.value}</span>
         <button>&#11109</button>
         <button onclick="editTask(this)">Edit</button>
 		<button onclick="deleteTask(this)"><i class="fa-solid fa-trash"></i></button>
     `;
 
-    taskList.appendChild(newTask);
-    taskInput.value = '';
+	taskList.appendChild(newTask);
+    
+	//clear all input information
+	taskInput.value = '';
+	taskLabel.value = '';
+	taskPriority.value = '';
+	taskDueDate.value = '';
 }
 
 function completeTask(button) {
@@ -48,8 +71,40 @@ function editTask(button) {
     }
 }
 
-
 function deleteTask(button) {
     var task = button.parentNode;
     task.parentNode.removeChild(task);
 }
+
+function checkLabel(taskLabel, color) {
+	var labelList = document.getElementById('labelList');
+	var listContainer = document.getElementById('listContainer');
+	
+	
+	if (labelList.textContent.includes(taskLabel.value)){
+		return;
+	}
+	else {	
+		//add new label as an option for future tasks
+		var newOption = document.createElement('option');
+		newOption.textContent = taskLabel.value;
+		labelList.appendChild(newOption);
+		
+		//create new list
+		listContainer.innerHTML += `
+		<div class="list" style="background-color:${color};">
+		<ul id="${taskLabel.value}">
+			<h3>${taskLabel.value}</h3>
+            <!-- Tasks will be added dynamically here -->
+		</ul>
+		</div>
+		`;
+		
+	}
+	
+}
+
+
+
+
+
